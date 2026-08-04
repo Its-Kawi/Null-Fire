3b574bf0b9907f4309fef0adb8ece38ae43c3c05
Null Cherry <sk4ndi1488@gmail.com>
2026-08-04 13:25:37 +0300

    Update Autoplay BETA.lua

--- Core/Loaders/Funky-Friday/Autoplay BETA.lua
+++ Core/Loaders/Funky-Friday/Autoplay BETA.lua
@@ -1,1 +1,4 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/Null-Cherry/Null-Fire/refs/heads/main/Core/Loaders/Funky-Friday/Autoplay.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Null-Cherry/Null-Fire/refs/heads/main/Core/Loaders/Funky-Friday/Autoplay.lua"))()
+lib.AutoPlay = true
+
+return lib
